cb7de6cea2efd9ba564df31ede77f3d86418357f
alex8396 <63743294+alex8396@users.noreply.github.com>
2025-02-13 16:11:11 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.04 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String myString) {
+        // myString을 소문자로 변환하여 answer에 저장
+        String answer = myString.toLowerCase();
+        return answer;
+    }
+}
