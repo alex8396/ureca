074b9d676ddbb75994b0fcf8f2cdfc1bdc7387b6
alex8396 <63743294+alex8396@users.noreply.github.com>
2025-02-18 17:18:29 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.14 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String[] solution(String my_string) {
+        // 공백을 기준으로 문자열을 나누어 배열로 반환
+        String[] answer = my_string.split(" ");
+        return answer;
+    }
+}
